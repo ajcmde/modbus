51ffd0fa2bff32798a9bd668dc164f3fae49bc8b
Alexander Eggerer <134808561+ajcmde@users.noreply.github.com>
2025-05-04 05:24:01 +0200

    Inital version
    read inverter registers, smart meters, batteries

---
+++ python/example1.py
@@ -0,0 +1,32 @@
+import modbus
+
+Configuration_UnitID = 1
+Configuration_IP = "wechselrichter1"
+Configuration_Port = 1502
+Configuration_Timeout = 1
+
+tcpmodbus = modbus.SolarEdge()
+tcpmodbus.tcp_connect(Configuration_IP, Configuration_Port, Configuration_Timeout)
+
+print("Inverter 1")
+print(tcpmodbus.Inverter(Configuration_UnitID))
+
+print("Inverter 2")
+print(tcpmodbus.Inverter(Configuration_UnitID + 1))
+
+print("Smart Meter 1")
+print(tcpmodbus.SmartMeter(Configuration_UnitID, 1))
+print("Smart Meter 2")
+print(tcpmodbus.SmartMeter(Configuration_UnitID, 2))
+print("Smart Meter 3")
+print(tcpmodbus.SmartMeter(Configuration_UnitID, 3))
+
+print("Battery 1")
+print(tcpmodbus.Battery(Configuration_UnitID, 1))
+print("Battery 2")
+print(tcpmodbus.Battery(Configuration_UnitID, 2))
+
+print ("Grid Protection Trip Limits")
+print(tcpmodbus.GridProtectionTripLimits(Configuration_UnitID))
+
+tcpmodbus.tcp_close()
