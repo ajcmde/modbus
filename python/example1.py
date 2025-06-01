c6af1640e4470346d786f7dbf6cc2fad01dbae78
Alexander Eggerer <134808561+ajcmde@users.noreply.github.com>
2025-06-01 20:28:07 +0200

    interface update, nocopyright added

--- python/example1.py
+++ python/example1.py
@@ -1,3 +1,29 @@
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# 
+# For more information, please refer to <https://unlicense.org>
+#
 
 import modbus
 from collections import namedtuple
@@ -18,22 +44,22 @@ for Block in SunSpecBlocks:
     print("------------------------------")
     print("BlockId: ", Block.BlockId, " Address: ", Block.Address, " Length: ", Block.Length)
 
-    print(tcpmodbus.read_sunspec_block(Configuration_UnitID, Block.Address, Block.BlockId))
+    print(tcpmodbus.ReadBlock(Configuration_UnitID, Block.Address, Block.BlockId))
 print("------------------------------")
 
-print("Smart Meter 1")
+print("SolarEdge SmartMeter 1")
 print(tcpmodbus.SmartMeter(Configuration_UnitID, 1))
-print("Smart Meter 2")
+print("SolarEdge SmartMeter 2")
 print(tcpmodbus.SmartMeter(Configuration_UnitID, 2))
-print("Smart Meter 3")
+print("SolarEdge SmartMeter 3")
 print(tcpmodbus.SmartMeter(Configuration_UnitID, 3))
 
-print("Battery 1")
+print("SolarEdge Battery 1")
 print(tcpmodbus.Battery(Configuration_UnitID, 1))
-print("Battery 2")
+print("SolarEdge Battery 2")
 print(tcpmodbus.Battery(Configuration_UnitID, 2))
 
-print ("Grid Protection Trip Limits")
+print ("SolarEdge Grid Protection Trip Limits")
 print(tcpmodbus.GridProtectionTripLimits(Configuration_UnitID))
 
 tcpmodbus.tcp_close()
