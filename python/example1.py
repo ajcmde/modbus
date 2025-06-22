fdfb14b1e357980407dd7827d4f381eafc0b6bfa
Alexander Eggerer <134808561+ajcmde@users.noreply.github.com>
2025-06-22 18:33:17 +0200

    clustering of examples

--- python/example1.py
+++ python/example1.py
@@ -33,33 +33,39 @@ Configuration_IP = "wechselrichter1"
 Configuration_Port = 1502
 Configuration_Timeout = 1
 
+
 tcpmodbus = modbus.SolarEdge()
 tcpmodbus.tcp_connect(Configuration_IP, Configuration_Port, Configuration_Timeout)
 
-print("SunSpec")
-sunspec = tcpmodbus.SunSpec(Configuration_UnitID)
-print(sunspec)
-SunSpecBlocks = tcpmodbus.SunSpec(Configuration_UnitID)
-for Block in SunSpecBlocks:
+if True:
+    print("SunSpec")
+    sunspec = tcpmodbus.SunSpec(Configuration_UnitID)
+    print(sunspec)
+    SunSpecBlocks = tcpmodbus.SunSpec(Configuration_UnitID)
+    for Block in SunSpecBlocks:
+        print("------------------------------")
+        print("BlockId: ", Block.BlockId, " Address: ", Block.Address, " Length: ", Block.Length)
+        if Block.BlockId == 101 or True:
+            print(tcpmodbus.ReadBlock(Configuration_UnitID, Block.Address, Block.BlockId))
     print("------------------------------")
-    print("BlockId: ", Block.BlockId, " Address: ", Block.Address, " Length: ", Block.Length)
 
-    print(tcpmodbus.ReadBlock(Configuration_UnitID, Block.Address, Block.BlockId))
-print("------------------------------")
+if True:
+    print("SolarEdge SmartMeter 1")
+    print(tcpmodbus.SmartMeter(Configuration_UnitID, 1))
+    print("SolarEdge SmartMeter 2")
+    print(tcpmodbus.SmartMeter(Configuration_UnitID, 2))
+    print("SolarEdge SmartMeter 3")
+    print(tcpmodbus.SmartMeter(Configuration_UnitID, 3))
 
-print("SolarEdge SmartMeter 1")
-print(tcpmodbus.SmartMeter(Configuration_UnitID, 1))
-print("SolarEdge SmartMeter 2")
-print(tcpmodbus.SmartMeter(Configuration_UnitID, 2))
-print("SolarEdge SmartMeter 3")
-print(tcpmodbus.SmartMeter(Configuration_UnitID, 3))
+if True:
+    print("SolarEdge Battery 1")
+    print(tcpmodbus.Battery(Configuration_UnitID, 1))
+if False:    
+    print("SolarEdge Battery 2")
+    print(tcpmodbus.Battery(Configuration_UnitID, 2))
 
-print("SolarEdge Battery 1")
-print(tcpmodbus.Battery(Configuration_UnitID, 1))
-print("SolarEdge Battery 2")
-print(tcpmodbus.Battery(Configuration_UnitID, 2))
-
-print ("SolarEdge Grid Protection Trip Limits")
-print(tcpmodbus.GridProtectionTripLimits(Configuration_UnitID))
+if True:
+    print ("SolarEdge Grid Protection Trip Limits")
+    print(tcpmodbus.GridProtectionTripLimits(Configuration_UnitID))
 
 tcpmodbus.tcp_close()
